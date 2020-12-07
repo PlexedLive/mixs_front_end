7e452ba7214ca5f6b42cde9fa300485466ca1ce4
Arron Ritchie <arronwritchie@gmail.com>
2020-12-07 21:38:13 +0900

    oops, broke aishus stuff, fixed now

--- app.py
+++ app.py
@@ -81,92 +81,94 @@ def app():
                 'name': stem_name
                 })
 
-            button = st.button("Proceed with Separation?")
 
 
-    if button:
+
+    if youtube_link != "":
         link = YouTubeTools(youtube_link)
-        link.clear_wavs()
-        youtube_display(youtube_link)
-        filename = link.get_audio_and_directory()
-        stems, rate = splitter(filename)
-        rate = int(rate)
+        button = st.button("Separate")
 
-        for stem, audio in stems.items():
-            np_audio(audio, stem, rate)
+        if button:
+            link.clear_wavs()
+            filename = link.get_audio_and_directory()
+            stems, rate = splitter(filename)
+            rate = int(rate)
 
-        # if stems != None:
-        #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Vocals</h3>",unsafe_allow_html=True)
-        #     np_audio(stems['vocals'], rate)
+            for stem, audio in stems.items():
+                np_audio(audio, stem, rate)
 
-        #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Piano</h3>",unsafe_allow_html=True)
-        #     np_audio(stems['piano'], rate)
+            # if stems != None:
+            #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Vocals</h3>",unsafe_allow_html=True)
+            #     np_audio(stems['vocals'], rate)
 
-
-        #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Drums</h3>",unsafe_allow_html=True)
-        #     # write('./media/drums.wav', rate, stems['drums'])
+            #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Piano</h3>",unsafe_allow_html=True)
+            #     np_audio(stems['piano'], rate)
 
 
-        #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Bass</h3>",unsafe_allow_html=True)
-        #     # write('./media/bass.wav', rate, stems['bass'])
+            #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Drums</h3>",unsafe_allow_html=True)
+            #     # write('./media/drums.wav', rate, stems['drums'])
 
 
-        #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Other</h3>",unsafe_allow_html=True)
-            # write('./media/other.wav', rate, stems['other'])
+            #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Bass</h3>",unsafe_allow_html=True)
+            #     # write('./media/bass.wav', rate, stems['bass'])
 
-    components.html(f'''
-                <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-<link rel="stylesheet" href="http://naomiaro.github.io/waveform-playlist/css/main.css">
-  <div id="top-bar" class="playlist-top-bar">
-<div class="playlist-toolbar">
-    <div class="btn-group">
-      <span class="btn-pause btn btn-warning"><i class="fa fa-pause"></i></span>
-      <span class="btn-play btn btn-success"><i class="fa fa-play"></i></span>
-      <span class="btn-stop btn btn-danger"><i class="fa fa-stop"></i></span>
-      <span class="btn-rewind btn btn-success"><i class="fa fa-fast-backward"></i></span>
-      <span class="btn-fast-forward btn btn-success"><i class="fa fa-fast-forward"></i></span>
+
+            #     st.markdown("<h3 style='text-align: left; color: white; font-family: Monaco'>Other</h3>",unsafe_allow_html=True)
+                # write('./media/other.wav', rate, stems['other'])
+
+            components.html(f'''
+                    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="http://naomiaro.github.io/waveform-playlist/css/main.css">
+      <div id="top-bar" class="playlist-top-bar">
+    <div class="playlist-toolbar">
+        <div class="btn-group">
+          <span class="btn-pause btn btn-warning"><i class="fa fa-pause"></i></span>
+          <span class="btn-play btn btn-success"><i class="fa fa-play"></i></span>
+          <span class="btn-stop btn btn-danger"><i class="fa fa-stop"></i></span>
+          <span class="btn-rewind btn btn-success"><i class="fa fa-fast-backward"></i></span>
+          <span class="btn-fast-forward btn btn-success"><i class="fa fa-fast-forward"></i></span>
+        </div>
+        <span class="audio-pos">00:00:00.0</span>
+      </div>
     </div>
-    <span class="audio-pos">00:00:00.0</span>
-  </div>
-</div>
-<div id='playlist' data-urls='{json.dumps(stem_urls)}'> </div>
+    <div id='playlist' data-urls='{json.dumps(stem_urls)}'> </div>
 
-<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-<script type="text/javascript" src="http://naomiaro.github.io/waveform-playlist/js/waveform-playlist.var.js"></script>
-<script type="text/javascript">
-  var playlist = WaveformPlaylist.init({{
-  samplesPerPixel: 10000,
-  mono: true,
-  waveHeight: 120,
-  timescale: true,
-  container: document.getElementById("playlist"),
-  state: "cursor",
-  colors: {{
-    waveOutlineColor: "#E0EFF1",
-    timeColor: "grey",
-    fadeColor: "black"
-  }},
-  controls: {{
-    show: true,
-    width: 150
-  }},
-  zoomLevels: [500, 1000, 3000, 5000, 10000]
-}});
+    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="http://naomiaro.github.io/waveform-playlist/js/waveform-playlist.var.js"></script>
+    <script type="text/javascript">
+      var playlist = WaveformPlaylist.init({{
+      samplesPerPixel: 10000,
+      mono: true,
+      waveHeight: 120,
+      timescale: true,
+      container: document.getElementById("playlist"),
+      state: "cursor",
+      colors: {{
+        waveOutlineColor: "#E0EFF1",
+        timeColor: "grey",
+        fadeColor: "black"
+      }},
+      controls: {{
+        show: true,
+        width: 150
+      }},
+      zoomLevels: [500, 1000, 3000, 5000, 10000]
+    }});
 
-playlist
-  .load(JSON.parse(document.getElementById("playlist").dataset.urls))
-  .then(function() {{
-    // can do stuff with the playlist.
-  }});
+    playlist
+      .load(JSON.parse(document.getElementById("playlist").dataset.urls))
+      .then(function() {{
+        // can do stuff with the playlist.
+      }});
 
 
-</script>
-<script type="text/javascript" src="http://naomiaro.github.io/waveform-playlist/js/emitter.js"></script>
+    </script>
+    <script type="text/javascript" src="http://naomiaro.github.io/waveform-playlist/js/emitter.js"></script>
 
-</script>
+    </script>
 
-''', height=1000)
+        ''', height=1000)
 
         else:
             youtube_display(youtube_link)
